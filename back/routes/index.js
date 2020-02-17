cfb91149633fd99972fe23bdbe35bb1de9a7fbc1
Nipsu96 <n.lahteinen1@gmail.com>
2020-02-17 11:46:17 +0200

    second

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
+
+const express = require('express')
 var router = express.Router();
+const app = express()
+const bodyParser = require('body-parser')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const config = require('../utils/config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+app.use(bodyParser.json())
+
+const options = config.DATABASE_OPTIONS;
+
+const knex = require('knex')(options);
+router.get('/score', (request, response) => {
+  // response.json(note)
+  knex.from('highscore').select("*").orderBy('score','ASC')
+    .then((rows) => {
+      console.log(rows)
+      response.json(rows)
+    })
+    .catch((err) => { console.log(err); throw err })
+
+})
+
+router.post('/score', (request, response) => {
+  const score = request.body
+  console.log(score)
+
+console.log(note);
+knex('highscore').insert(score)
+  .then((id) => {
+    console.log("more data inserted")
+    score.id = id[0];
+    response.json(score)
+  })
+  .catch((err) => {
+     console.log(err); 
+ response.status(500).json(
+   {error: "database error in insert"}
+ ) })
+})
 module.exports = router;
